91ae31b6e47249350c7f7d474ffb51adc8cc7f71
VoblaSmile <vobla@smile.fr>
2016-01-12 11:00:01 +0200

    add style? translate and js

---
+++ src/app/code/Smile/Report/view/frontend/web/js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+/*jshint browser:true jquery:true*/
+define([
+    'jquery',
+    'jquery/ui'
+], function($){
+    "use strict";
+    $(document).ready(function(){
+       alert('dfgdfg111111');
+    });
+    debugger;
+    console.log('dfgdfg');
+    return $.mage.reportScript;
+});
